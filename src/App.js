3f9f52633c9b68dba098ac8676b68d0fc520c345
Yachika30 <yachikankumar@gmail.com>
2022-01-11 00:47:06 +0530

    updated signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup'
-
+import {BrowseRouter, BrowserRouter} from 'react-router-dom'
 function App() {
   return (
+    <BrowserRouter>
     <Signup/>
+    </BrowserRouter>
   );
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,16 +2,18 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
 import TextField from '@mui/material/TextField';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import Alert from '@mui/material/Alert';
 import './Signup.css'
 import logo from '../Assets/logo.png'
+import {Link} from "react-router-dom"
+import { typography } from '@mui/system';
 export default function Login() {
+  
+ 
   return (
     <div className='signupwrapper'>
       <div className='signupcard'>
@@ -22,7 +24,7 @@ export default function Login() {
      </div>
       
       <CardContent>
-      <Typography variant="body2" color="text.secondary">
+      <Typography textAlign='center' color= 'gray'>
           Signup to enjoy short videos
         </Typography>
       {true && <Alert severity="error">This is an error alert — check it out!</Alert>} 
@@ -49,27 +51,40 @@ export default function Login() {
           id="standard-password-input"
           label="Password"
           type="password"
+          margin='dense'
           autoComplete="current-password"
           variant="standard"
           fullWidth = {true}
         />
-         <Button size="small" fullWidth = {true} variant='outlined' margin="normal" startIcon={<CloudUploadIcon/>} component='label'>
+         <Button size="small" fullWidth = {true} variant='outlined' margin='dense' startIcon={<CloudUploadIcon/>} component='label'>
            upload profile picture
 
          <input type='file' accept='image/*' hidden></input>
          </Button>
       </div>
-         <Button color='primary'  variant='contained' fullWidth={true} >
+         <Button color='primary' className='button' variant='contained' fullWidth={true} >
            Sign Up
          </Button>
-        
+        <Typography className='Type1'>
+          By signing in you agree to our terms and conditions
+        </Typography>
       </CardContent>
       <CardActions>
         
         
       </CardActions>
     </Card>
-
+    <div className='Card2'>
+    <Card variant='outlined'>
+     
+      <CardContent >
+      <Typography textAlign='center' color= 'gray'>
+         Having an account? <Link to="/login"> Log in</Link>
+        </Typography>
+      </CardContent>
+     
+    </Card>
+    </div>
     </div>
     </div>
 
